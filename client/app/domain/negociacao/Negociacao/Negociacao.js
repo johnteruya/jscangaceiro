7ebe6373d6bb69b918a5fcd3f81376ddff31deff
johnteruya <jteruya9@gmail.com>
2019-01-18 22:41:53 -0200

    ate exercício 1

---
+++ client/app/domain/negociacao/Negociacao/Negociacao.js
@@ -0,0 +1,8 @@
+class Negociacao{
+    constructor(data, quantidade, valor){
+        //cada parametro recebido será atribuido as propriedades da classe
+        this.data = data;
+        this. quantidade = quantidade;
+        this.valor = valor;
+    }
+}
