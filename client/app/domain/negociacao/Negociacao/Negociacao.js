8683ca615e00469f2293ee58dc8fd85218ec3ec7
johnteruya <jteruya9@gmail.com>
2019-01-22 22:45:32 -0200

    até o cap. 2.5 encapsulamento

--- client/app/domain/negociacao/Negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao/Negociacao.js
@@ -1,12 +1,28 @@
 class Negociacao{
     constructor(data, quantidade, valor){
         //cada parametro recebido será atribuido as propriedades da classe
-        this.data = data;
-        this. quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this. _quantidade = quantidade;
+        this._valor = valor;
     }
 
-    obtemVolume(){
-        return this.quantidade * this.valor;
+    //criando o método Volume
+    getVolume(){
+        return this._quantidade * this._valor;
+    }
+
+    //criando o método Data
+    getData(){
+        return this._data;
+    }
+
+    //criando o método Quantidade
+    getQuantidade(){
+        return this._quantidade;
+    }
+
+    //criando o método Valor
+    getValor(){
+        return this._valor;
     }
 }
