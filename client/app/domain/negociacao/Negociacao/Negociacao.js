082495299ad004e0e77bdf13f40c11b78bf443d2
johnteruya <jteruya9@gmail.com>
2019-01-24 23:27:27 -0200

    exercício feito até o cap. 2.12

--- client/app/domain/negociacao/Negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao/Negociacao.js
@@ -1,28 +1,27 @@
 class Negociacao{
-    constructor(data, quantidade, valor){
-        //cada parametro recebido será atribuido as propriedades da classe
-        this._data = data;
-        this. _quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor){
+        Object.assign(this, {_quantidade, _valor});
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
     }
 
     //criando o método Volume
-    getVolume(){
+    get volume(){
         return this._quantidade * this._valor;
     }
 
     //criando o método Data
-    getData(){
-        return this._data;
+    get data(){
+        return new Date(this._data.getTime());
     }
 
     //criando o método Quantidade
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade;
     }
 
     //criando o método Valor
-    getValor(){
+    get valor(){
         return this._valor;
     }
 }
